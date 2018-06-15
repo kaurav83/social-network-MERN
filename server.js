f8261fbf02a283838f277e984900c93852af8999
Vitaliy <puga.vitaliy@gmail.com>
2018-06-15 16:04:04 +0300

    сделал регистрацию пользователей на стороне сервера

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
 const users = require("./routes/api/users");
 const profile = require("./routes/api/profile");
 const posts = require("./routes/api/posts");
 
 const app = express();
+
+// mongoose.Promise = global.Promise;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 const db = require("./config/keys").mongoURI;
 
@@ -19,6 +25,10 @@ app.get("/", (req, res) => res.send("Hello wd"));
 app.use("/api/users", users);
 app.use("/api/profile", profile);
 app.use("/api/posts", posts);
+
+process.on("unhandledRejection", up => {
+  throw up;
+});
 
 const port = process.env.PORT || 5000;
 
