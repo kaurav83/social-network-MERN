d7972eafcee31e21019fa12c27cd3efe494e77d9
Vitaliy <puga.vitaliy@gmail.com>
2018-06-17 00:44:59 +0300

    api для профиля пользователя: создание и редактирование профиля, описание для основных полей, соцсетей, навыков

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -31,4 +31,61 @@ router.get('/', passport.authenticate('jwt', { session: false }), (req, res) => 
         .catch(err => res.status(400).json(err));
 });
 
+//@route    POST api/profile
+//@desc     create or edit user profile
+//@access   Private
+router.post('/', passport.authenticate('jwt', { session: false }), (req, res) => {
+    // GET fields
+    const profileFields = {};
+    profileFields.user = req.user.id;
+    if (req.body.handle) profileFields.handle = req.body.handle;
+    if (req.body.company) profileFields.company = req.body.company;
+    if (req.body.website) profileFields.website = req.body.website;
+    if (req.body.location) profileFields.location = req.body.location;
+    if (req.body.status) profileFields.status = req.body.status;
+    // Скилы делим внутри массива согласно схеме в модели
+    if (typeof req.body.skills !== 'undefined') {
+        profileFields.skills = req.body.skills.split(',');
+    }
+
+    if (req.body.bio) profileFields.bio = req.body.bio;
+    if (req.body.githubsername) profileFields.githubsername = req.body.githubsername;
+
+    // Соцсети
+    profileFields.social = {};
+    if (req.body.youtube) profileFields.social.youtube = req.body.youtube;
+    if (req.body.twitter) profileFields.social.twitter = req.body.twitter;
+    if (req.body.facebook) profileFields.social.facebook = req.body.facebook;
+    if (req.body.vk) profileFields.social.vk = req.body.vk;
+    if (req.body.instagram) profileFields.social.instagram = req.body.instagram;
+    if (req.body.linkedin) profileFields.social.linkedin = req.body.linkedin;
+
+    Profile.findOne({ user: req.user.id })
+        .then(profile => {
+            if (profile) {
+                // update
+                Profile.findOneAndUpdate(
+                    { user: req.user.id },
+                    { $set: profileFields },
+                    { new: true }
+                ).then(profile => res.json(profile));
+
+            } else {
+                // create
+
+                // check if handle exists
+                Profile.findOne({ handle: profileFields.handle }).then(profile => {
+                    if (profile) {
+                        errors.handle = 'Этот обработчик уже существуем';
+                        res.status(400).json(errors);
+                    }
+
+                    // save profile
+                    new Profile(profileFields).save().then(profile => res.json(profile));
+                })
+            }
+        })
+
+});
+
 module.exports = router;
