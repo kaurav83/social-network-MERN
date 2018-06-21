b7f3a58e1778567f2232a4fc349b75f301eb1122
Vitaliy <puga.vitaliy@gmail.com>
2018-06-21 23:41:43 +0300

    сверстал страницу регистрации, написал состояние для полей и повесил на них обработчики, немного изменил внешний вид ссылок в хедере

--- client/src/components/Layout/Header.js
+++ client/src/components/Layout/Header.js
@@ -1,9 +1,29 @@
 import React, { Component } from 'react'
-import { NavLink, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
 import './Header.css';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#fff9c4',
+      main: '#ffeb3b',
+      dark: '#ffc107',
+      contrastText: '#000',
+    },
+    secondary: {
+      light: '#CCFF90',
+      main: '#fff',
+      dark: '#64DD17',
+      contrastText: '#000',
+    },
+  },
+});
 
 class Header extends Component {
   render() {
+
     return (
       <section className="header">
         <nav className="navbar">
@@ -12,33 +32,45 @@ class Header extends Component {
           </div>
           <ul className="navbar-list">
             <li className="navbar-list__item">
-              <NavLink
-                to="/profiles"
-                className="navbar-list__link"
-                activeClassName="selected"
-              >
-                Разработчики
-              </NavLink>
+              <MuiThemeProvider theme={theme}>
+                <Button
+                  href="/profiles"
+                  className="navbar-list__link"
+                  color="secondary"
+                  variant="text"
+                  style={{ textTransform: "capitalize", fontSize: "1.2rem" }}
+                >
+                  Разработчики
+              </Button>
+              </MuiThemeProvider>
             </li>
           </ul>
           <ul className="navbar-list">
             <li className="navbar-list__item">
-              <NavLink
-                to="/register"
-                className="navbar-list__link sign-up"
-                activeClassName="selected"
-              >
-                Зарегистрироваться
-              </NavLink>
+              <MuiThemeProvider theme={theme}>
+                <Button
+                  href="/register"
+                  className="navbar-list__link sign-up"
+                  color="secondary"
+                  variant="text"
+                  style={{ textTransform: "capitalize", fontSize: "1.2rem" }}
+                >
+                  Зарегистрироваться
+              </Button>
+              </MuiThemeProvider>
             </li>
             <li className="navbar-list__item">
-              <NavLink
-                to="/login"
-                className="navbar-list__link sign-in"
-                activeClassName="selected"
-              >
-                Войти
-              </NavLink>
+              <MuiThemeProvider theme={theme}>
+                <Button
+                  href="/login"
+                  className="navbar-list__link sign-in"
+                  color="secondary"
+                  variant="text"
+                  style={{ textTransform: "capitalize", fontSize: "1.2rem" }}
+                >
+                  Войти
+                </Button>
+              </MuiThemeProvider>
             </li>
           </ul>
         </nav>
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,13 +1,144 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { withStyles, MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import Input from '@material-ui/core/Input';
+import Button from '@material-ui/core/Button';
+import purple from '@material-ui/core/colors/purple';
+import './Register.css';
+
+const styles = theme => ({
+  cssUnderline: {
+    '&:after': {
+      borderBottomColor: purple[500]
+    }
+  }
+});
+
+const theme = createMuiTheme({
+  palette: {
+      primary: {
+          light: '#fff9c4',
+          main: '#ffeb3b',
+          dark: '#ffc107',
+          contrastText: '#000',
+      },
+      secondary: {
+          light: '#CCFF90',
+          main: '#76FF03',
+          dark: '#64DD17',
+          contrastText: '#000',
+      },
+  },
+});
 
 class Register extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+      password: '',
+      password2: '',
+      email: '',
+      errors: {}
+    }
+
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  onChange(e) {
+    e.preventDefault();
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+    const newUser = {
+      name: this.state.name,
+      email: this.state.email,
+      password: this.state.password,
+      password2: this.state.password2
+    }
+    console.log(newUser)
+  }
+
   render() {
+    const {classes} = this.props;
     return (
-      <div>
-        Register
-      </div>
+      <section className="register">
+        <div className="register-container">
+          <div className="register-content">
+          <h1 className="register-title">Зарегистрироваться</h1>
+          <p className="register-lead">Создайте ваш аккаунт</p>
+          <form onSubmit={this.onSubmit}>
+            <div className="form-group">
+              <Input
+                type="text"
+                className="form-control"
+                placeholder="Name"
+                name="name"
+                value={this.state.name}
+                onChange={this.onChange}
+                classes={{
+                  underline: classes.cssUnderline
+                }}
+              />
+            </div>
+            <div className="form-group">
+              <Input
+                type="email"
+                className="form-control"
+                placeholder="Email address"
+                name="email"
+                value={this.state.email}
+                onChange={this.onChange}
+                classes={{
+                  underline: classes.cssUnderline
+                }}
+              />
+            </div>
+            <div className="form-group">
+              <Input
+                type="password"
+                className="form-control"
+                placeholder="Password"
+                name="password"
+                value={this.state.password}
+                onChange={this.onChange}
+                classes={{
+                  underline: classes.cssUnderline
+                }}
+              />
+            </div>
+            <div className="form-group">
+              <Input
+                type="password"
+                className="form-control"
+                placeholder="Confirm password"
+                name="password2"
+                value={this.state.password2}
+                onChange={this.onChange}
+                classes={{
+                  underline: classes.cssUnderline
+                }}
+              />
+            </div>
+            <MuiThemeProvider theme={theme}>
+              <Button 
+                  variant="contained" 
+                  type="submit" 
+                  color="primary" 
+                  style={{width: "100%", margin: ".5rem 0 0 0"}}
+                >Отправить
+              </Button>
+            </MuiThemeProvider>
+          </form>
+          </div>
+        </div>
+      </section>
     )
   }
 }
 
-export default Register;
+export default withStyles(styles)(Register);
