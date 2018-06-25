a3a22352c16e1ce32c63f02fed17178f92ed7eda
Vitaliy <puga.vitaliy@gmail.com>
2018-06-25 12:43:40 +0300

    продолжаю работу над профилем и админкой. Подтянул данные через redux в админку, сделал переиспользующийся спиннер для загрузки данных

---
+++ client/src/components/Spinner.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import purple from '@material-ui/core/colors/purple';
+
+const styles = theme => ({
+  progress: {
+    margin: theme.spacing.unit * 2,
+  },
+});
+
+function Spinner(props) {
+  const { classes } = props;
+  return (
+    <div>
+      <CircularProgress className={classes.progress} size={50} thickness={7} />
+    </div>
+  );
+}
+
+Spinner.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Spinner);
--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import {Link} from 'react-router-dom';
 
 import {connect} from 'react-redux';
 import {getCurrentProfile} from '../../actions/profileActions';
+import Spinner from '../Spinner';
 
 class Dashboard extends Component {
     componentDidMount() {
@@ -10,18 +12,53 @@ class Dashboard extends Component {
     }
 
     render() {
-        return (
-            <div>
+        const {user} = this.props.auth;
+        const {profile, loading} = this.props.profile;
 
+        let dashboardContent; 
+
+        if (profile === null || loading) {
+            dashboardContent = <h4><Spinner /></h4>
+        } else {
+            // делаем проверку, имеет ли залогированный пользователь данные профиля
+            if (Object.keys(profile).length > 0) {
+                dashboardContent = <h4>TODO: DISPLAY PROFILE</h4>
+            } else {
+                // пользователь залогирован но у него нет профиля
+                dashboardContent = (
+                    <div>
+                        <p className="lead lead--muted">Привет, {user.name}</p>
+                        <p>Вы еще не установили профиль, пожалуйста, добавьте какую то информацию</p>
+                        <Link to="/create-profile">
+                            Создать профиль
+                        </Link>
+                    </div>
+                );
+            }
+        }
+
+        return (
+            <div className="dashboard">
+                <div className="dashboard-container">
+                    <h1 className="dashboard-title">Dashboard</h1>
+                    {dashboardContent}
+                </div>
             </div>
         )
     }
 }
 
-// const mapStateToProps = (state) => {
-//     return {
-//         profile: state.profile
-//     }
-// }
+Dashboard.propTypes = {
+    getCurrentProfile: PropTypes.func.isRequired,
+    auth: PropTypes.object.isRequired,
+    profile: PropTypes.object.isRequired
+}
 
-export default connect(null, {getCurrentProfile})(Dashboard);
+const mapStateToProps = (state) => {
+    return {
+        profile: state.profile,
+        auth: state.auth
+    }
+}
+
+export default connect(mapStateToProps, {getCurrentProfile})(Dashboard);
