39b74af900332d0422cedd5891bb3e0d8cfc1277
Vitaliy <puga.vitaliy@gmail.com>
2018-06-30 09:28:17 +0300

    поднятнул данные в разметку компонента ProfileAbout

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -30,7 +30,7 @@ class Profile extends Component {
                         </Link>
                     </div>
                     <ProfileHeader profile={profile} />
-                    <ProfileAbout />
+                    <ProfileAbout  profile={profile} />
                     <ProfileCreds />
                     <ProfileGithub />
                 </div>
--- client/src/components/profile/ProfileAbout.js
+++ client/src/components/profile/ProfileAbout.js
@@ -1,13 +1,43 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import isEmpty from '../../validation/is_empty';
 
 class ProfileAbout extends Component {
-  render() {
-    return (
-      <div>
-        <h1>Profile about</h1>
-      </div>
-    )
-  }
+    render() {
+        const {profile} = this.props;
+
+        //получаем имя
+        const firstName = profile.user.name.trim().split(' ')[0];
+
+        // список навыков
+        const skills = profile.skills.map((skill, index) => {
+            return (
+                <div key={index} className="headerAbout-skill">
+                    <i className="fa fa-check"> {skill}</i>
+                </div>
+            )
+        })
+
+        return (
+            <div className="headerAbout-container">
+                <div className="headerAbout-content">
+                    <h3 className="text-center text-info">{firstName} о себе</h3>
+                    <p className="headerAbout-text-lead">
+                        {isEmpty(profile.bio) ? 
+                            <span>{firstName} ничего о себе пока что не написал</span> 
+                            : 
+                            (<span>{profile.bio}</span>)
+                        }
+                    </p>
+                    <hr/>
+                    <h3 className="text-center text-info">Навыки</h3>
+                    <div className="headerAbout-skills">
+                        {skills}
+                    </div>
+                </div>
+            </div>
+        )
+    }
 }
 
-export default  ProfileAbout;
+export default ProfileAbout;
